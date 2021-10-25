9dd5b3af667a025b39571dc05bdaa64005c41610
Nicolo <you@example.com>
2021-10-25 22:08:34 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="columns">
+    <div class="column">
+        <div class="trt">
+      <img src="https://cdn.discordapp.com/attachments/746763110278365255/899365942411280434/Untitled-221.png" alt="julia"/>  
+      <p>Cuck-O-Meter is a top of the line cuckhold test, it is used to determine whether you are/turning into a cuckhold. The process is slow, but very dangerous, once a a person has been infected with cuckness, it's very hard to turn back. But everything is possible, this test will help you determine you level of cuckness. According to your score we will determine your current state (cuck genome levels in your organism) and probable plan of action. Good luck and stay alpha!</p>
+        </div>
+    </div>
+
+  <div class="column">     
+<div class="form-wrapper">
+  <form onsubmit="return false;">
+    <hgroup>
+      <h1>Get Started</h1>
+      <h3>Test Your Cuck-level <a href="https://twitter.com/hakimel" tabindex="-1">@hakimel</a>.</h3>
+    </hgroup>
+
+    <label for="name">Name</label>
+    <input type="text" id="name">
+    
+    <label for="email">Expected Cuck Points(out of 100)</label>
+    <input type="text" id="cuck">
+
+    <div class="checkbox">
+      <input type="checkbox" id="check1">
+      <label for="check1">I accept the terms</label><br>
+    </div>
+    <button>Submit</button>
+  </form>
+</div>
+</div>
+</div>
+
+</body>
+</html>
